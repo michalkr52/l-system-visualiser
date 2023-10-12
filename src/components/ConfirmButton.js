0c2c6a282567293305e97cc6e831f02dcdb2d003
michalkr52 <46329932+michalkr52@users.noreply.github.com>
2023-10-12 13:55:49 +0200

    added keybinds

--- src/components/ConfirmButton.js
+++ src/components/ConfirmButton.js
@@ -1,9 +1,25 @@
 import "./styles/ConfirmButton.css";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AlgorithmContext } from "../contexts/AlgorithmContext";
 
 function ConfirmButton() {
     const { rules, axiom, confirmed, setConfirmed, onConfirm } = useContext(AlgorithmContext);
+
+    const handleKeyDown = (event) => {
+        if (event.key === "Enter" && !confirmed) {
+            onConfirm();
+        }
+        else if (event.key === "Escape" && confirmed) {
+            setConfirmed(false);
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener("keydown", e => handleKeyDown(e));
+        return () => {
+            document.removeEventListener("keydown", e => handleKeyDown(e));
+        }
+    }, [confirmed, rules, axiom]);
 
     if (confirmed) {
         return (
--- src/components/StepControls.js
+++ src/components/StepControls.js
@@ -1,10 +1,26 @@
 import "./styles/StepControls.css";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AlgorithmContext } from "../contexts/AlgorithmContext";
 
 function StepControls() {
     const { confirmed, displayedStep, incrementDisplayedStep, decrementDisplayedStep } = useContext(AlgorithmContext);
 
+    const handleKeyPress = (event) => {
+        if (!confirmed) return;
+        if (event.key === "ArrowLeft") {
+            decrementDisplayedStep();
+        } else if (event.key === "ArrowRight") {
+            incrementDisplayedStep();
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener("keydown", handleKeyPress);
+        return () => {
+            document.removeEventListener("keydown", handleKeyPress);
+        }
+    }, [confirmed, displayedStep]);
+    
     return (
         <div className="settings-content align-bottom">
             <div className="step-controls-title">Step</div>
