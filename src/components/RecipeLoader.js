b1f6528cd11bd52c3fabb0af0547190e73694d82
michalkr52 <46329932+michalkr52@users.noreply.github.com>
2023-10-23 02:55:10 +0200

    recipes - basic component template, json file

---
+++ src/components/RecipeLoader.js
@@ -0,0 +1,13 @@
+import "./styles/RecipeLoader.css";
+import { useContext } from "react";
+import { ReactModal } from "react-modal";
+import { DrawingSettingsContext } from "../contexts/DrawingSettingsContext";
+import { AlgorithmContext } from "../contexts/AlgorithmContext";
+
+function RecipeLoader() {
+    const recipes = null;
+
+    return;
+}
+
+export default RecipeLoader;
